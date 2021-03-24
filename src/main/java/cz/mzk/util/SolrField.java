093c41a69f7d03a5c3ae8ece65ee937e341972f3
Aleksei Ermak <alexej.ermak96@gmail.com>
2021-03-24 14:51:28 +0100

    Change the Solr field name which means root uuid

--- src/main/java/cz/mzk/util/SolrField.java
+++ src/main/java/cz/mzk/util/SolrField.java
@@ -4,7 +4,7 @@ public class SolrField {
     public static final String UUID = "PID";
     public static final String DNNT = "dnnt";
     public static final String TIMESTAMP = "timestamp";
-    public static final String ROOT_UUID = "root_uuid";
+    public static final String ROOT_UUID = "root_pid";
     public static final String MODEL_PATH = "model_path";
     public static final String MODIFIED_DATE = "modified_date";
 }
